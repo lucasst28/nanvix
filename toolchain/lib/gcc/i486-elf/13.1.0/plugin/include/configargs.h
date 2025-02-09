816d2aa0842a279c99b17e2f5f86b5e49c9fba67
lucasst28 <lucasseverino1@hotmail.com>
2025-02-09 10:46:49 -0300

    Premier commit

---
+++ toolchain/lib/gcc/i486-elf/13.1.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../configure --target=i486-elf --prefix=/home/lucas-severino/Documents/AS/nanvix/toolchain --disable-nls --enable-languages=c --without-headers --disable-multilib";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "i486" }, { "arch", "i486" } };
